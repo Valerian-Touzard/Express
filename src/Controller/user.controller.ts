4d64ff976ceda6b70448d64eb638b05e74de222a
Valerian Touzard <vtouzard244@gmail.com>
2022-08-18 09:45:38 +0200

    Ajout de la documentation

--- src/Controller/user.controller.ts
+++ src/Controller/user.controller.ts
@@ -10,32 +10,51 @@ export class userController {
         this.model = model;
     }
 
-    /*
-    Récupère l'ensemble des users
+    /**
+     * Retourne la liste de tous les utilisateurs envoyés depuis le modèle.
+     *
+     * @param res - La réponse de la requête qui sera envoyé à la vue
+     *
+     * @author Valérian Touzard
      */
     getAllUser = async (req: Request, res:Response) => {
         const listUser = await this.model.getAllUser();
         res.render('user', {listUser});
     }
 
-    /*
-    Récupère les infos d'un user en fonction de son id
+    /**
+     * Retourne les information d'un utilisateurs, en fonction de son id
+     *
+     * @param req - Les données récupérées via l'id placer dans l'url
+     * @param res - La réponse de la requête qui sera envoyé à la vue
+     *
+     * @author Valérian Touzard
      */
     getOneUser = async (req: Request, res:Response) =>{
         const unUser = await this.model.getUnUser(req.params.id);
         res.render('detail', {unUser});
     }
 
-    /*
-    Supprime un utilisateur en fonction de sont id
+    /**
+     * Envoie une requête de suppression au models via l'id placé dans l'URL
+     *
+     * @param req - La requête de suppression envoyé au modèle
+     * @param res - Affichage de la vue deleteSuccess
+     *
+     * @author Valérian Touzard
      */
     deleteUser = async (req: Request, res:Response) =>{
-        const listUser = await this.model.deleteUser(req.params.id);
-        res.render('delete', {listUser});
+        await this.model.deleteUser(req.params.id);
+        res.render('delete');
     }
 
-    /*
-    Ajoute un nouvelle utilisateur en prenant les valeurs du formulaire
+    /**
+     * Envoie une requête de création au models via les informations récupérés du formulaire
+     *
+     * @param req - La requête de création envoyé au modèle avec les informations du formulaire
+     * @param res - Redirection vers l'URL addSuccess
+     *
+     * @author Valérian Touzard
      */
 
     createUser =  async(req: Request, res:Response) =>{
@@ -47,29 +66,66 @@ export class userController {
 
     }
 
+    /**
+     * Affiche la vue addSuccess
+     *
+     * @param req - La requête
+     * @param res - Affichage de la vue addSuccess
+     *
+     * @author Valérian Touzard
+     */
     success = (req: Request, res:Response) =>{
         res.render('addSuccess')
     }
 
-    /*
-        Affiche le formulaire
-    */
+    /**
+     * Affiche le formulaire de création
+     *
+     * @param req - La requête
+     * @param res - Affichage de la vue formulaire
+     *
+     * @author Valérian Touzard
+     */
     getForm = (req: Request, res:Response) =>{
         res.render('formulaire');
     }
 
 
+    /**
+     * Affiche le formulaire de création
+     *
+     * @param req - La requête qui permet de récupérer les information a afficher dan le formulaire d'un utilisateur (via son id)
+     * @param res - Affichage de la vue modification avec les informations de l'utilisateur à modifier
+     *
+     * @author Valérian Touzard
+     */
     formModifUser = async (req: Request, res:Response) =>{
         const userAModif = await this.model.getUnUser(req.params.id);
         res.render('modification', {userAModif});
     }
 
+    /**
+     * Envoie une requête de modification au models via les informations récupérés du formulaire de modification
+     *
+     * @param req - La requête qui permet de récupérer les informations à modifier
+     * @param res - Redirige vers l'URL updateSuccess
+     *
+     * @author Valérian Touzard
+     */
     updateUser = async(req: Request, res:Response)=>{
         const valid = await this.model.updateUser(req.body)
             .then(response => response.status);
         res.redirect(`/updateSuccess`);
     }
 
+    /**
+     * Affiche la vue updateSuccess
+     *
+     * @param req - La requête
+     * @param res - Affichage de la vue updateSuccess
+     *
+     * @author Valérian Touzard
+     */
     successModif = (req: Request, res:Response) =>{
         res.render('updateSuccess');
     }
--- src/Router/user.router.ts
+++ src/Router/user.router.ts
@@ -8,15 +8,15 @@ const model = new userModel();
 const controller = new userController(model);
 
 // Les routes
-router.get('/', controller.getAllUser);
-router.get('/detail/:id', controller.getOneUser);
-router.get('/delete/:id', controller.deleteUser);
+router.get('/', controller.getAllUser);             // Page d'Accueil
+router.get('/detail/:id', controller.getOneUser);   // Page de détail d'un user
+router.get('/delete/:id', controller.deleteUser);   // Page de suppression d'un user
 
-router.get('/add', controller.getForm);
-router.post('/traitement', controller.createUser);
-router.get('/addSuccess', controller.success);
+router.get('/add', controller.getForm);                 // Page du formulaire de création
+router.post('/traitement', controller.createUser);      // Page de traitement des datas récupérées du formulaire
+router.get('/addSuccess', controller.success);          // Page de succès lors de la création du user
 
-router.get('/modified/:id', controller.formModifUser);
-router.post('/traitementModif', controller.updateUser);
-router.get('/updateSuccess', controller.successModif);
+router.get('/modified/:id', controller.formModifUser);      // Page du formulaire de modification
+router.post('/traitementModif', controller.updateUser);     // Page de traitement des datas récupérées du formulaire
+router.get('/updateSuccess', controller.successModif);      // Page de succès lors de la modification du user
 export default router;
